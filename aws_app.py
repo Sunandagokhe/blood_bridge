c079abfadc63c5b510d886c014266f345fc86baa
Sunanda Gokhe <sunandagokhe02@gmail.com>
2026-02-15 19:04:48 +0530

    Remove empty lines at the beginning of aws_app.py

--- aws_app.py
+++ aws_app.py
@@ -1,5 +1,3 @@
-
-
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from datetime import datetime, timedelta
 from werkzeug.security import generate_password_hash, check_password_hash
